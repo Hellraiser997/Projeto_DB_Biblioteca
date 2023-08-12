b42698900f3199b45e42faca935738bd9b0f48d7
Hellraiser997 <felipesg03@gmail.com>
2023-08-12 10:46:04 -0300

    Fix: Registros restantes preenchidos

--- script/TRABALHO-BD-BIBLIOTECA-EMPRESTIMO-DATA.sql
+++ script/TRABALHO-BD-BIBLIOTECA-EMPRESTIMO-DATA.sql
@@ -24,3 +24,21 @@ VALUES
 ("A Profecia do Livro Antigo", "3135578888809", "1988"),
 ("O Enigma do Relógio", "4045512692420", "1973");
 SELECT * FROM livros;
+
+INSERT INTO mutuarios (primeiro_nome, sobrenome, email_contato, id_livros_fk)
+VALUES
+("Robson", "Vicente", "RobVice@gmail.com", 1),
+("Carolina", "Almeida", "CarolAlmeida@gmail.com", 2),
+("Pedro", "Barroso", "PedroBarroso@gmail.com", 3),
+("Ana", "Macedo", "AnaMacedi@gmail.com", 4),
+("Fernando", "Siqueira", "FernandoSiqueira@gmail.com", 5);
+SELECT * FROM mutuarios;
+
+INSERT INTO emprestimos (data_emprestimo, data_devolucao, id_livros_fk, id_mutuario_fk)
+VALUES
+("2020-07-23", "2020-08-23", 1, 1),
+("2015-05-12", "2015-06-12", 2, 2),
+("2021-09-01", "2021-10-01", 3, 3),
+("2019-03-07", "2019-03-07", 4, 4),
+("2018-08-18", "2018-09-18", 5, 5);
+SELECT * FROM emprestimos;
